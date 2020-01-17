0b1effa40b149541812974ab5ecbd89d72ac97f2
Movindsouza <51433382+Movindsouza@users.noreply.github.com>
2020-01-17 20:24:43 +0530

    Add files via upload

---
+++ DogVsCat_BNN.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import cv2
+import numpy as np
+import os
+from random import shuffle
+from tqdm import tqdm
+import larq as lq
+import tensorflow as tf
+
+TRAIN_DIR='M:/dataset/train/train'
+TEST_DIR='M:/dataset/test/'
+IMG_SIZE= 100
+LR =1e-3
+
+MODEL_NAME='dogvscat.model'
+
+
+# In[36]:
+
+
+def label_img(img):
+    word_label=img.split('.')[0]
+    print(word_label)
+    if word_label == 'cat':return [1,0]
+    elif word_label == 'dog' : return [0,1]
+
+
+# In[37]:
+
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+
+# In[38]:
+
+
+def process_test_data():
+    testing_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
+        img_num = img.split('.')[0]
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        testing_data.append([np.array(img), img_num])
+        
+    shuffle(testing_data)
+    np.save('test_data.npy', testing_data)
+    return testing_data
+
+
+# In[39]:
+
+
+train_data =create_train_data()
+
+
+# In[54]:
+
+
+# All quantized layers except the first will use the same options
+kwargs = dict(input_quantizer="ste_sign",
+              kernel_quantizer="ste_sign",
+              kernel_constraint="weight_clip")
+
+model = tf.keras.models.Sequential()
+
+# In the first layer we only quantize the weights and not the input
+model.add(lq.layers.QuantConv2D(32, (3, 3),
+                                kernel_quantizer="ste_sign",
+                                kernel_constraint="weight_clip",
+                                use_bias=False,
+                                input_shape=(100, 100, 1)))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+
+model.add(lq.layers.QuantConv2D(64, (3, 3), use_bias=False, **kwargs))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+
+model.add(lq.layers.QuantConv2D(128, (3, 3), use_bias=False, **kwargs))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+
+model.add(lq.layers.QuantConv2D(256, (3, 3), use_bias=False, **kwargs))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+
+model.add(lq.layers.QuantConv2D(512, (3, 3), use_bias=False, **kwargs))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(lq.layers.QuantDense(1024, use_bias=False, **kwargs))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+model.add(lq.layers.QuantDense(2, use_bias=False, **kwargs))
+model.add(tf.keras.layers.BatchNormalization(scale=False))
+model.add(tf.keras.layers.Activation("softmax"))
+
+
+# In[55]:
+
+
+lq.models.summary(model)
+
+
+# In[56]:
+
+
+train = train_data[:20000]
+test = train_data[-5000:]
+
+
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = np.array([i[1] for i in train])
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y =np.array( [i[1] for i in test])
+
+
+
+
+# In[58]:
+
+
+print(type(test_x))
+print(type(X))
+
+
+# In[59]:
+
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+# In[60]:
+
+
+
+model.fit(X, Y, batch_size=128, epochs=35,verbose=1)
+
+
+# In[61]:
+
+
+test_loss, test_acc = model.evaluate(test_x, test_y)
+test_loss, test_acc 
+
+
+# In[12]:
+
+
+
+
+
+# In[13]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
